2584938301915009f792bcdfd6499f01417aee12
Gokul <gokulchandan18@gmail.com>
2021-12-28 21:33:30 +0530

    commit

--- src/app/components/add-student/add-student.component.ts
+++ src/app/components/add-student/add-student.component.ts
@@ -17,12 +17,14 @@ addStudent= new FormGroup({
 
 });
 
+message: boolean=false;
   ngOnInit(): void { 
   }
 SaveData(){ 
   //console.log(this.addStudent.value);
   this.student.saveStudentData(this.addStudent.value).subscribe((result)=>{
-    console.log(result);
+    //console.log(result);
+    this.message=true;
   });
 }
 }
