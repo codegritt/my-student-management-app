0269bd567c743c8ee45840ea98f2071707765f6b
Gokul <gokulchandan18@gmail.com>
2021-12-29 09:02:52 +0530

    commit

--- src/app/components/edit-student/edit-student.component.ts
+++ src/app/components/edit-student/edit-student.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl } from '@angular/forms';
+import { StudentsService } from 'src/app/students.service';
+
 
 @Component({
   selector: 'app-edit-student',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditStudentComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private student:StudentsService) { }
+  addStudent= new FormGroup({
+    first_name: new FormControl(''),
+    last_name:new FormControl(''),
+    email: new FormControl('')
+  
+  });
+  
+  message: boolean=false;
+    ngOnInit(): void { 
+    }
+  SaveData(){ 
+    //console.log(this.addStudent.value);
+    this.student.saveStudentData(this.addStudent.value).subscribe((result)=>{
+      //console.log(result);
+      this.message=true;
+      this.addStudent.reset({});
+    });
+  }
+  removeMessage(){
+    this.message=false;
   }
 
 }
