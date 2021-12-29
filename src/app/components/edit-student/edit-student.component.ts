b5d5810b20f96cbc06b2339625dfabe8a8f335d1
Gokul <gokulchandan18@gmail.com>
2021-12-29 13:06:00 +0530

    commit

--- src/app/components/edit-student/edit-student.component.ts
+++ src/app/components/edit-student/edit-student.component.ts
@@ -21,22 +21,14 @@ export class EditStudentComponent implements OnInit {
 
   message: boolean = false;
   ngOnInit(): void {
-    //console.log(this.router.snapshot.params['id']);
-    this.student.getStudentById(this.router.snapshot.params['id']).subscribe((result: any) => {
-      //console.log(result);
-      this.editStudent = new FormGroup({
-        first_name: new FormControl(result['first_name']),
-        last_name: new FormControl(result['last_name']),
-        email: new FormControl(result['email']),
-
-      });
-    });
+    console.log(this.router.snapshot.params['id']);
+    
+  
 
 
   }
   UpdateData() {
-    console.log(this.editStudent.value);
-
+   
   }
   removeMessage() {
     this.message = false;
